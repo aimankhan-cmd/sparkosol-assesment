0d6654d499663774a769e416faf675988eb1c774
aimankhan-cmd <111960457+aimankhan-cmd@users.noreply.github.com>
2023-01-28 06:16:40 -0800

    Update model.py

--- app/model.py
+++ app/model.py
@@ -1,19 +1,5 @@
 from pydantic import BaseModel, Field, EmailStr
 
-#POST schema
-class PostSchema(BaseModel):
-    id: int = Field(default=None)
-    title: str = Field(...)
-    content: str = Field(...)
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "title": "Post add only with JWT.",
-                "content": "We'll be using PyJWT to perform Task assessment"
-            }
-        }
-
 
 #User schema
 class UserSchema(BaseModel):
